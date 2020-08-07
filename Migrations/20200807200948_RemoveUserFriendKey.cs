a166d47e3914562899cc17d9984bc0758f5781ac
Daouda Camara <dcdaoudakaleyire@gmail.com>
2020-08-08 05:16:28 +0900

    FIX: db migration

--- Migrations/20200807200948_RemoveUserFriendKey.cs
+++ Migrations/20200807200948_RemoveUserFriendKey.cs
@@ -14,10 +14,31 @@ namespace tymbot.Migrations
                 name: "IX_UserFriend_UserId",
                 table: "UserFriend",
                 column: "UserId");
+
+                migrationBuilder.CreateIndex(
+                name: "IX_UserFriend_FriendId",
+                table: "UserFriend",
+                column: "FriendId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFriend_User_FriendId",
+                table: "UserFriend",
+                column: "FriendId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFriend_User_FriendId",
+                table: "UserFriend");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserFriend_FriendId",
+                table: "UserFriend");
+                
             migrationBuilder.DropIndex(
                 name: "IX_UserFriend_UserId",
                 table: "UserFriend");
